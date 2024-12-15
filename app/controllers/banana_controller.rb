d92289960b302d685a4e0536cee9020ffafe78be
Grace Do <61570746+grace1501@users.noreply.github.com>
2024-12-15 02:28:31 +0000

    testing first routes

--- app/controllers/banana_controller.rb
+++ app/controllers/banana_controller.rb
@@ -1,5 +1,6 @@
 class BananaController < ApplicationController
   def apple
+    @random_move = ["rock", "paper", "scissors"].sample
     render({ :template => "game_templates/play_rock"})
   end
 end
