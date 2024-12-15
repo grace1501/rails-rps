50d614e0a7b86c7024dc65fd3f2c39ad86cec7c3
Grace Do <61570746+grace1501@users.noreply.github.com>
2024-12-15 03:21:27 +0000

    route scissors done

--- app/controllers/scissors_controller.rb
+++ app/controllers/scissors_controller.rb
@@ -1,5 +1,9 @@
 class ScissorsController < ApplicationController
   def play_scissors
+    @your_move = "scissors"
+    @comp_move = @moves.sample
+    @result = get_result(@your_move, @comp_move)
+    
     render({ :template => "game_templates/play_scissors"})
   end
 end
